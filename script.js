a5ab38396140661500872207c5a8f911f10acfcb
leilei-anthony <lesteranthonyjr@gmail.com>
2025-05-05 21:09:14 +0800

    ADD functions are working

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const Gameboard = function() {
     const board = new Array(9).fill('');
 
-        function placeMark(index, mark) {
+        function placeToken(index, token) {
             if(board[index] === '') {
-                board[index] = mark;
+                board[index] = token;
                 return true;
             }
             return false;
@@ -20,7 +20,7 @@ const Gameboard = function() {
         }
 
         return {
-            placeMark,
+            placeToken,
             getBoard,
             resetBoard
         }
@@ -57,29 +57,106 @@ const Player = function() {
     }
 }
 
-const CheckVictory = function() {
+const CheckWin = function() {
     let result = {};
 
-    const row = function(board) {
+    const checkRow = function(board, token) {
         result = {};
 
-
+        if((board[0] === token &&
+            board[1] === token &&
+            board[2] === token) ||
+           (board[3] === token &&
+            board[4] === token &&
+            board[5] === token) ||
+           (board[6] === token &&
+            board[7] === token &&
+            board[8] === token)
+        ) {
+            result = {winner: token};
+        }
     }
 
-    const column = function(board) {
+    const checkColumn = function(board, token) {
         result = {};
+
+        if((board[0] === token &&
+            board[3] === token &&
+            board[6] === token) ||
+           (board[1] === token &&
+            board[4] === token &&
+            board[7] === token) ||
+           (board[2] === token &&
+            board[5] === token &&
+            board[8] === token)
+        ) {
+            result = {winner: token};
+        }
     }
 
-    const diagonal = function(board) {
+    const checkDiagonal = function(board, token) {
         result = {};
+
+        if((board[0] === token &&
+            board[4] === token &&
+            board[8] === token) ||
+           (board[2] === token &&
+            board[4] === token &&
+            board[6] === token)
+        ) {
+            result = {winner: token};
+        }
     }
 
-    const tie = function(board) {
-        result = {};
+    const getResult = function() {
+        return result;
+    }
+
+    return {
+        checkRow,
+        checkColumn,
+        checkDiagonal,
+        getResult
     }
 }
 
 const GameControl = function() {
+    const gb = Gameboard();
+    const pl = Player();
+    const cw = CheckWin();
+
+    pl.addPlayer('p1', 'X');
+    pl.addPlayer('p2', 'O');
+
+    const p1 = pl.getPlayers(0);
+    const p2 = pl.getPlayers(1);
+
+    console.log(gb.getBoard());
+
+    console.log(p1);
+    console.log(p2);
+
+    gb.placeToken(0, p1.token)
+    console.log(gb.getBoard());
+
+    gb.placeToken(3, p2.token)
+    console.log(gb.getBoard());
+
+    gb.placeToken(1, p1.token)
+    console.log(gb.getBoard());
+
+    gb.placeToken(4, p2.token)
+    console.log(gb.getBoard());
+
+    gb.placeToken(2, p1.token)
+    console.log(gb.getBoard());
+
+    cw.checkRow(gb.getBoard(), p1.token);
+    console.log(cw.getResult());
+
+
+
 
 }
 
+GameControl();
